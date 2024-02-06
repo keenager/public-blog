3e9969429da8a283907078d74aa8d6c4bc483b0d
keenager <keenager84@gmail.com>
2024-02-07 02:00:23 +0900

    remarkBreak plugin 추가

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,8 +1,13 @@
 import { remark } from "remark";
 import remarkGfm from "remark-gfm";
+import remarkBreak from "remark-breaks";
 import html from "remark-html";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).use(remarkGfm).process(markdown);
+  const result = await remark()
+    .use(remarkGfm)
+    .use(remarkBreak)
+    .use(html)
+    .process(markdown);
   return result.toString();
 }
