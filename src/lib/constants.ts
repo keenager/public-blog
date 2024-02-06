f89e51db7685b512bce3525a6b4e48770779caa1
keenager <keenager84@gmail.com>
2024-02-07 01:12:46 +0900

    일기 태그 추가

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,4 +9,5 @@ export const tagBlogRouteList = [
   { href: "/blog/tags/book", name: "책" },
   { href: "/blog/tags/dev", name: "웹개발" },
   { href: "/blog/tags/travel", name: "여행" },
+  { href: "/blog/tags/essay", name: "일기" },
 ];
