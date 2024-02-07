a6d93789b456f90948d46998951cfed82066be5e
keenager <keenager84@gmail.com>
2024-02-08 01:59:16 +0900

    remark-autolink-headings 적용

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -3,7 +3,10 @@ import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import remarkBreak from "remark-breaks";
 import remarkRehype from "remark-rehype";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeStringify from "rehype-stringify";
+import { h } from "hastscript";
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
@@ -11,6 +14,13 @@ export default async function markdownToHtml(markdown: string) {
     .use(remarkGfm)
     .use(remarkBreak)
     .use(remarkRehype)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, {
+      properties: { class: "anchor", "aria-hidden": "true", tabindex: "-1" },
+      content(node) {
+        return h("span.octicon-link");
+      },
+    })
     .use(rehypeStringify)
     .process(markdown);
   return result.toString();
